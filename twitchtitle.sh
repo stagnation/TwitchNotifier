d82da0970d9c22d23c3c7b65541b4e16b3f04598
Nils <nils@wireklint.se>
2015-04-10 19:02:30 +0200

    first stab at channel title

---
+++ twitchtitle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#if [ $# -eq 2 ] ; then
+#output='tmpdl'
+output=$(date +%s.%N)
+#if [ $# -ge 1 ] ; then
+
+    echo 'ok'
+    wget $1 -O $output --quiet
+    titlestring=$(grep "property='og:description'>" $output)
+    echo $titlestring |  sed "s/<meta content='//" | sed "s/' property='og:description'>//"
+    rm $output
+#fi
